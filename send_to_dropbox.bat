7619e8a936df30881a19ef067e8e052802f96e33
Leodnls <leo.daniels@2026.icam.fr>
2024-12-02 17:17:04 +0100

    Delete send_to_dropbox.bat

--- send_to_dropbox.bat
+++
@@ -1,5 +0,0 @@
-curl -X POST https://content.dropboxapi.com/2/files/upload ^
---header "Authorization: Bearer sl.CB0fJF54Q9SbDUSU3enAdPrVa42L7Yf5bPbuH6PmqONAwoe17ZucbVN4Co6BiyLvyoQ9J_LePV7JLNs1FKf6jpnPdPFGl8Eu93-eax_vW20CH0fkkCX8zveLAiXd2T_ThdTMityyF9ec" ^
---header "Dropbox-API-Arg: {\"path\": \"/arbo.txt\", \"mode\": \"add\", \"autorename\": true, \"mute\": false}" ^
---header "Content-Type: application/octet-stream" ^
---data-binary @arbo.txt
